e741ed59716a25fec93f7b33026dcbeee037900b
Anatoly Nikiforov <anikiforov@nvidia.com>
2016-06-21 13:46:10 +0300

    * qtcreator/upd-creator-files.sh: a bit better, but not the best;

--- qtcreator/upd-creator-files.sh
+++ qtcreator/upd-creator-files.sh
@@ -5,12 +5,12 @@ THISDIR=$(readlink -f $(dirname $0))
 OUT=`find $THISDIR -maxdepth 1 -iname *.files | tail -1`
 [ "$OUT" ] || { echo "Error: no *.files files found in this directory. Bailout"; exit 1; }
 
-EXCLUDE="\
-"
+EXCLUDE=" \
+    "
 
 PRUNE_ARGS=
 for arg in $EXCLUDE; do
-    PRUNE_ARGS="$PRUNE_ARGS -path $arg -prune -o"
+    PRUNE_ARGS="$PRUNE_ARGS -path $THISDIR/$arg -prune -o"
 done
 
-find $THISDIR $PRUNE_ARGS -type f -print | sed -n 's/^..//p' > $OUT && echo "OK"
+find $THISDIR $PRUNE_ARGS -type f -print > $OUT && echo "OK ;)" || echo "Failed :("
